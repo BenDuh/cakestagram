6d5259027ec6271d16a778dd768d8e59c90c0ca7
syl20 <sylvainmagana@ymail.com>
2019-05-14 13:55:44 +0200

    Auto stash before merge of "master" and "origin/master"

--- App/containers/TestContainer.js
+++ App/containers/TestContainer.js
@@ -16,12 +16,16 @@ class TestContainer extends Component {
                 <Button
                 title="Press Me"
                 onPress={getBoss}/>
+<<<<<<< Updated upstream
                 <Button
                 title="Login"
                 onPress={this.toLogin}/>
             </View>
                 
             
+=======
+            </View>   
+>>>>>>> Stashed changes
         );
     }
 }
