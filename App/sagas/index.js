25f93412df898760cf42cab5fdcbe53b01f44ddd
Max <m.vandine@live.fr>
2019-05-15 16:07:48 +0200

    saga correct

--- App/sagas/index.js
+++ App/sagas/index.js
@@ -6,7 +6,6 @@ import { getPostSaga } from './homeSaga';
 import { CONVERSATION_GET_REQUEST } from '../redux/reducers/conversationReducer'
 import { SESSION_GET_REQUEST } from '../redux/reducers/sessionReducer'
 import { SIGNUP_GET_REQUEST } from '../redux/reducers/signupReducer'
-import { getTest } from './applicationSaga'
 import { conversationGet } from './conversationSaga'
 import { USER_GET_REQUEST } from '../redux/reducers/userReducer';
 import {userGet} from './userSaga'
@@ -17,7 +16,7 @@ import { signUpGet } from './signupSaga'
 
 export default function * rootSaga () {
   yield all([
-    takeLatest(APPLICATION_ACTION_REQUEST, getTest),
+    // takeLatest(APPLICATION_ACTION_REQUEST, getTest),
     takeEvery(GET_POSTS_REQUEST, getPostSaga),
     takeLatest(CONVERSATION_GET_REQUEST, conversationGet),
     takeLatest(SESSION_GET_REQUEST, sessionGet),
