7577ab699c62e26d1747d1417581ecdde874feed
Raynaud <raynaud.sn@free.fr>
2019-05-15 11:49:53 +0200

    Ajout Moment pour format de date

--- App/containers/SinglePost.js
+++ App/containers/SinglePost.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Image, Dimensions } from 'react-native';
 import { Avatar, Icon } from 'react-native-elements';
-
+import moment from 'moment' // Pour formatter les dates
 
 const imgBase = "https://news.nationalgeographic.com/content/dam/news/2018/05/17/you-can-train-your-cat/02-cat-training-NationalGeographic_1484324.ngsversion.1526587209178.adapt.1900.1.jpg";
 
@@ -18,8 +18,8 @@ class SinglePost extends Component {
 
         const ownerFirstname = post ? this.props.post.owner.first_name : "Vide"
 
-        const dateCreation = post ? this.props.post.created_at : "Date"
-
+        const dateCreation = post ? moment(post.created_at).calendar() : "Date"
+        
         return (
             <View>
                 {avatar}
