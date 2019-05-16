a3db8e2e7fb4b898ae96a37db43f97c7c30437e6
Ben <duhamel.benoit@outllok.fr>
2019-05-16 14:13:23 +0200

    Navigation Bottom

--- App/navigation/NavigationApp.js
+++ App/navigation/NavigationApp.js
@@ -1,4 +1,4 @@
-import { createStackNavigator, createAppContainer, createBottomTabNavigator, createSwitchNavigator } from "react-navigation"
+import { createStackNavigator, createAppContainer, createBottomTabNavigator, createSwitchNavigator, TabBarBottom } from "react-navigation"
 import LoginContainer from "../containers/LoginContainer";
 import TestContainer from "../containers/TestContainer";
 import HomeContainer from "../containers/HomeContainer";
@@ -6,6 +6,8 @@ import SinglePostDetails from "../containers/SinglePostDetails";
 import ConversationContainer from "../containers/ConversationContainer";
 import SignupContainer from "../containers/SignupContainer";
 import UserContainer from '../containers/UserContainer';
+import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import ChatContainer from "../containers/ChatContainer";
 
 const AppNavigator = createStackNavigator({
@@ -32,6 +34,35 @@ const AppNavigator = createStackNavigator({
         headerLayoutPreset: 'center',
     })
 
+const ConversationNavigator = createStackNavigator({
+    Conversation: {
+        screen: ConversationContainer,
+        navigationOptions: {
+            title: 'Conversation'
+        }
+    },Chat: {
+        screen: ChatContainer,
+        nagigationOptions: {
+            title: 'Chat'
+        }
+    }
+    }, {
+        initialRouteName: 'Conversation',
+        headerLayoutPreset: 'center',
+})
+
+const ProfileNavigator = createStackNavigator({
+    Users: {
+        screen: UserContainer,
+        navigationOptions: {
+            title: 'Users'
+        }
+    }
+    }, {
+        initialRouteName: 'Users',
+        headerLayoutPreset: 'center',
+})
+
 const HomeNavigator = createStackNavigator({
     Home: {
         screen: HomeContainer,
@@ -45,22 +76,10 @@ const HomeNavigator = createStackNavigator({
             title: 'Commentaires du post'
         }
     },
-    Conversation: {
-        screen: ConversationContainer,
-        navigationOptions: {
-            title: 'Conversation'
-        }
-    },
-    Users: {
+    Profile: {
         screen: UserContainer,
         navigationOptions: {
             title: 'Users'
-        }
-    },
-    Chat: {
-        screen: ChatContainer,
-        nagigationOptions: {
-            title: 'Chat'
         }
     }
 }, {
@@ -68,13 +87,48 @@ const HomeNavigator = createStackNavigator({
         headerLayoutPreset: 'center',
     })
 
+const TabNavigator =  createBottomTabNavigator({
+        Home: HomeNavigator,
+        Conversation: ConversationNavigator,
+        Profil: ProfileNavigator
+    },
+    {defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+            const { routeName } = navigation.state;
+            let iconName;
+            let color;
+            if (routeName === 'Home') {
+                iconName = `home`
+                color = `${focused ? 'white' : '#2bcbba'}`
+            } else if (routeName === 'Conversation') {
+                iconName = 'comment-dots'
+                color = `${focused ? 'white' : '#2bcbba'}`
+            } else if (routeName === 'Profil') {
+                iconName = 'user'
+                color = `${focused ? 'white' : '#2bcbba'}`
+            }
+        return <Icon name={iconName} size={24} color={color} />}
+    }),
+    tabBarOptions: {
+        activeTintColor: 'white',
+        activeBackgroundColor : "#2bcbba",
+        labelStyle: {
+            fontSize: 14,
+        },
+        style: {
+            backgroundColor: 'white',
+        },
+        },
+      initialRouteName : 'Home'
+    });
 const AppBottom = createSwitchNavigator({
     LoginStack: AppNavigator,
-    HomeStack: HomeNavigator,
+    HomeStack: TabNavigator,
 },
     {
     initialRouteName: 'LoginStack',
     }
 );
+
 
 export default createAppContainer(AppBottom);
