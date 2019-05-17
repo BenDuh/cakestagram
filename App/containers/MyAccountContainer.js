5fd2b9cfc890d653c527acd06a3b173ace6f6f0a
Ben <duhamel.benoit@outllok.fr>
2019-05-17 11:57:35 +0200

    Auto stash before merge of "master" and "origin/master"

--- App/containers/MyAccountContainer.js
+++ App/containers/MyAccountContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button,Text, View } from 'react-native' 
+import { Button,Text, View, Image } from 'react-native' 
 import {connect} from 'react-redux'
 import { getMyAccountRequest } from '../redux/reducers/myAccountReducer'
 import { getEditRequest } from '../redux/reducers/editProfilReducer'
@@ -7,15 +7,18 @@ import Profil from '../components/profil'
 import Form from '../components/form'
 import MyInput from '../components/myInput'
 import { Formik } from 'formik'
+import ImagePicker from 'react-native-image-crop-picker'
+import RNFetchBlob from 'rn-fetch-blob'
 
 let INITIAL_VALUES={
     nom : '',
     prenom:''
-
 }
 class MyAccountContainer extends React.Component {
     state={
-        showForm: false
+        showForm: false, 
+        showImage: false,
+        image: {}
     }
     componentDidMount(){
         this.props.getMyAccount()
@@ -34,10 +37,21 @@ prenom: this.props.account.first_name,
     onSubmit = (e)=>{
       this.props.getMyEdit(e)
     }
+    selectPhoto=()=>{
+        ImagePicker.openPicker({
+            width: 300,
+            height: 400,
+            cropping: true
+          }).then(image => {
+            this.setState({showImage:true, image: image}),
+            console.log(image);
+          });
+          
+    }
     
     
     render() {
-        console.log('edit', this.props)
+        console.log('edit', this.state.imagepath)
         return (
             <View>
                 <Profil user={this.props.account}/>
@@ -58,12 +72,25 @@ prenom: this.props.account.first_name,
                                     <MyInput label="Nom" placeHolder="Entrez votre nom" name="nom" type="text" />
                                     <MyInput label="Prénom" placeHolder="Entrez votre prénom" name="prenom" type="text" />
                                     <MyInput label="Password" placeHolder="Entrez votre nouveau password " name="password" type="password" />
+                                    {this.state.showImage &&
+                                        <Image
+                                            style={{width: 100, height: 100}}
+                                            source={{uri :this.state.image.path}}
+                                        />
+                                    }
+                                    
+                                    
+                                    <Button 
+                                        title="Choisir une image"
+                                        onPress={this.selectPhoto}
+                                    />
                                     <View style={{ alignItems: 'center' }}>
                                         <Button
                                             title="Editer"
                                             onPress={handleSubmit}
                                             buttonStyle={{ backgroundColor: '#4b6584', width: 200, marginBottom: 20 }} />
                                     </View>
+                                    
 
                                 </Form>
                             )
